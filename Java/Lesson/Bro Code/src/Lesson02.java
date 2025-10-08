ceb9f163b6a132b16ea44e3b0dff012f543afcbd
mohdrashadi-pro <moalrashadi@gmail.com>
2025-10-08 09:20:22 +0800

    first lesson java Bro Code

---
+++ Java/Lesson/Bro Code/src/Lesson02.java
@@ -0,0 +1,53 @@
+public class Lesson02 {
+
+    public static void main(String[] args){
+/*
+
+Variable = a a reusable  a container for a value
+            a variable behaves as if it was the value it contains
+
+
+            - Primitive = simple value stored directly in memory (stack)
+            -Reference = memore address (Stack) that points to the (heap)
+
+
+             Primitive vs Reference
+             ---------    ---------
+             int          string
+             double       array
+             char         object
+             boolean 
+
+
+             * 2 steps to creating a variable
+             -------------------------------
+             1.declaration
+             2. assignment
+ */
+
+
+
+
+
+
+        int age = 12;
+        int year = 2025;
+
+
+        double price = 19.99;
+        double gpa = 3.5;
+        char grade ='A';
+        char symbol ='!';
+        char currency ='$';
+
+        boolean isStudent = true;
+
+
+        System.out.print(isStudent);
+
+
+
+
+
+    }
+}
---
+++ Java/Lesson/Bro Code/src/main.java
@@ -0,0 +1,20 @@
+public class main {
+
+    public static void main(String[] args){
+
+        // hello world
+        /*
+
+        This is a mutlipe comment
+         */
+
+
+        System.out.print("I like pizza! \n");
+        System.out.print("It's realy good ");
+        System.out.print("\nBuy me pizza ");
+
+
+    }
+
+
+}
